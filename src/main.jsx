f032deb7fd5ae46f48803be077ccc109b02cf5c0
Zahid Hasan <imzahidhasan.mail@gmail.com>
2024-07-17 01:39:11 +0600

    project initialized

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Register from './components/authentication/Register.jsx'
+import Dashboard from './components/dashboard/Dashboard.jsx'
+
+
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/register',
+    element:<Register/>
+  },
+  {
+    path: '/dashboard',
+    element:<Dashboard/>
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </React.StrictMode>,
+)
